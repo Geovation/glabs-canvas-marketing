b286021f614d12e2935f4410790e885318ed105f
James Gardner <james@jimmyg.org>
2018-10-23 13:15:34 +0100

    Delete support

--- app/dropboxsync.js
+++ app/dropboxsync.js
@@ -37,6 +37,7 @@ shell.mkdir('-p', STATE_PATH)
 const dbx = new Dropbox({ accessToken: DROPBOX_ACCESS_TOKEN, fetch })
 const writeFileAsync = promisify(fs.writeFile)
 const readFileAsync = promisify(fs.readFile)
+const unlinkAsync = promisify(fs.unlink)
 
 // Webhook
 
@@ -44,17 +45,6 @@ const setupDropboxWebhook = (app) => {
   // You can verify the authenticity of the request by looking at the X-Dropbox-Signature header, which will contain the HMAC-SHA256 signature of the entire request body using your app secret as the key.
   // MUST come before body-parser so we can check the signature
   // https://stackoverflow.com/questions/9920208/expressjs-raw-body
-  app.use(function (req, res, next) {
-    var data = ''
-    req.setEncoding('utf8')
-    req.on('data', function (chunk) {
-      data += chunk
-    })
-    req.on('end', function () {
-      req.rawBody = data
-      next()
-    })
-  })
   app.all('/webhook', async (req, res, next) => {
     if (req.method === 'GET' && req.query.hasOwnProperty('challenge')) {
       console.log(req.query['challenge'])
@@ -62,36 +52,39 @@ const setupDropboxWebhook = (app) => {
       res.append('X-Content-Type-Options', 'nosniff')
       res.send(req.query['challenge'])
     } else {
-      const hmac = crypto.createHmac('SHA256', DROPBOX_SECRET)
-      hmac.update(req.rawBody)
-      const hash = hmac.digest('hex')
-      if (req.get('X-Dropbox-Signature') === hash) {
-        const hook = JSON.parse(req.rawBody)
-        console.log(hook)
-        if (DROPBOX_APP_ID && hook.list_folder.accounts[0] !== DROPBOX_APP_ID) {
-          console.error('Unknown App ID')
-          res.status(500)
-          res.send('Unknown App ID')
+      let data = ''
+      req.setEncoding('utf8')
+      req.on('data', function (chunk) {
+        data += chunk
+      })
+      req.on('end', async () => {
+        const hmac = crypto.createHmac('SHA256', DROPBOX_SECRET)
+        hmac.update(data)
+        const hash = hmac.digest('hex')
+        if (req.get('X-Dropbox-Signature') === hash) {
+          const hook = JSON.parse(data)
+          console.log(hook)
+          if (DROPBOX_APP_ID && hook.list_folder.accounts[0] !== DROPBOX_APP_ID) {
+            console.error('Unknown App ID')
+            res.status(500)
+            res.send('Unknown App ID')
+          } else {
+            await dropboxStatefulSync()
+            res.send('OK')
+          }
         } else {
-          await statefullSync()
-          res.send('OK')
+          console.error('Invalid Signature')
+          res.status(500)
+          res.send('Invalid Signature')
         }
-      } else {
-        console.error('Invalid Signature')
-        res.status(500)
-        res.send('Invalid Signature')
-      }
+      })
     }
   })
 }
 
 const syncOne = async (remotePath) => {
-  const localPath = path.join(DROPBOX_LOCAL_FOLDER_PATH, remotePath.slice(DROPBOX_REMOTE_FOLDER_PATH.length, remotePath.length))
-  const parts = localPath.split('/')
-  const filename = parts[parts.length - 1]
-  const dir = parts.slice(0, parts.length - 1).join('/')
+  const {dir, filename, dest} = remoteToLocal(remotePath)
   shell.mkdir('-p', dir)
-  const dest = path.join(dir, filename)
   let fileDownload
   try {
     fileDownload = await dbx.filesDownload({ path: remotePath })
@@ -120,34 +113,40 @@ const getChanges = async (cursor) => {
       body: JSON.stringify({ cursor })
     }
   )
-  let result = {}
+  let deleted = {}
+  let changed = {}
   console.log(res.status)
   if (res.status !== 200) {
     throw new Error(await res.text())
   }
-  const json = await res.json()
-  // console.log('JSON', json)
+  const text = await res.text()
+  const json = JSON.parse(text)
+  console.log('JSON', json)
   for (let i = 0; i < json.entries.length; i++) {
-    const { content_hash, id } = json.entries[i]
-    const contentHash = content_hash
+    const contentHash = json.entries[i].content_hash
+    const id = json.entries[i].id
     if (json.entries[i]['.tag'] === 'file') {
       if (json.entries[i].path_display.startsWith(DROPBOX_REMOTE_FOLDER_PATH)) {
-        result[json.entries[i].path_display] = { contentHash, id }
+        changed[json.entries[i].path_display] = { contentHash, id }
         // console.log(json.entries[i].path_display)
       } else {
         console.log('Skipping', json.entries[i].path_display)
       }
+    } else if (json.entries[i]['.tag'] === 'deleted') {
+      console.log('DELETED', json.entries[i].path_display)
+      deleted[json.entries[i].path_display] = { contentHash, id }
     }
   }
-  console.log(result)
+  console.log(deleted)
   cursor = json.cursor
-  console.log('Result', result)
+  // console.log('Result', changed)
   if (json.has_more) {
     const next = await getChanges(cursor)
-    result = Object.assign(result, next.changes)
+    changed = Object.assign(changed, next.changed)
+    deleted = Object.assign(deleted, next.deleted)
     cursor = next.cursor
   }
-  return { 'changes': result, 'cursor': cursor }
+  return { changed, cursor, deleted}
 }
 
 const getCursor = async () => {
@@ -170,12 +169,12 @@ const getCursor = async () => {
         body
       }
     )
-    console.log(res.status)
+    console.log(res.statusText)
     if (res.status !== 200) {
       throw new Error(await res.text())
     }
-    const json = await res.json()
-    // console.log(json)
+    const text = await res.text()
+    const json = JSON.parse(text)
     return json.cursor
   }
 }
@@ -185,14 +184,39 @@ const setCursor = async (cursor) => {
   await writeFileAsync(path.join(STATE_PATH, 'cursor.json'), JSON.stringify(cursor), { encoding: 'utf8' })
 }
 
+const remoteToLocal = (remotePath) => {
+  const localPath = path.join(DROPBOX_LOCAL_FOLDER_PATH, remotePath.slice(DROPBOX_REMOTE_FOLDER_PATH.length, remotePath.length))
+  const parts = localPath.split('/')
+  const filename = parts[parts.length - 1]
+  const dir = parts.slice(0, parts.length - 1).join('/')
+  const dest = path.join(dir, filename)
+  return {dest, dir, filename}
+}
+
+const deleteOne = async (remotePath) => {
+  console.log(`Need to delete ${remotePath}`)
+  const {dest} = remoteToLocal(remotePath)
+  await unlinkAsync(dest)
+  console.log(`Deleted ${dest}`)
+  return true
+}
+
 const syncDropbox = async (startCursor) => {
-  const { changes, cursor } = await getChanges(startCursor)
-  console.log('==== Got new cursor', cursor)
-  console.log(changes)
-  const promises = []
-  for (var changedPath in changes) {
-    if (changes.hasOwnProperty(changedPath)) {
-      promises.push((async function (changedPath) {
+  const { changed, deleted, cursor } = await getChanges(startCursor)
+  console.log('Got new cursor', cursor)
+  console.log('Got deleted', deleted)
+
+  const deletes = []
+  for (let deletedPath in deleted) {
+    if (deleted.hasOwnProperty(deletedPath)) {
+      deletes.push(deleteOne(deletedPath))
+    }
+  }
+
+  const downloads = []
+  for (let changedPath in changed) {
+    if (changed.hasOwnProperty(changedPath)) {
+      downloads.push((async function (changedPath) {
         const success = await syncOne(changedPath)
         if (success) {
           console.log(`Synced ${changedPath}`)
@@ -205,15 +229,17 @@ const syncDropbox = async (startCursor) => {
     }
   }
   console.log('Returning new cursor', cursor)
-  return { 'cursor': cursor, 'downloads': promises }
+  return { cursor, downloads, deletes }
 }
 
 const dropboxStatefulSync = async () => {
   const startCursor = await getCursor()
   console.log('Got cursor', startCursor)
   // We assume that the call above only sends the folder itself
-  const { cursor, downloads } = await syncDropbox(startCursor)
-  console.log(cursor, downloads)
+  const { cursor, downloads, deletes } = await syncDropbox(startCursor)
+  console.log('Waiting for deletes', deletes)
+  await Promise.all(deletes)
+  console.log('Waiting for downloads', downloads)
   await Promise.all(downloads)
   console.log('Saving cursor to disk', cursor)
   await setCursor(cursor)
